9a2c8f75a123da5b94284aca7b42039350fc63c0
BBeak <bkh3441@gmil.com>
2022-05-25 10:18:06 +0900

    검색 기능 추가

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,6 +3,9 @@ package Main;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+
+
 import util.Util;
 import dto.Articles;
 
@@ -41,25 +44,36 @@ public class Main {
 				list.add(article);
 				System.out.printf("%d번째 게시물이 작성되었습니다.\n", id);
 
-			} else if (command.equals("article list")) {
-				if (list.size() == 0) {
-					System.out.println("현재 등록된 게시물이 없습니다.");
-				}
-				if (list.size() != 0) {
-					System.out.printf("현재 %d개의 게시물이 있습니다.\n", lastarticleId);
-					System.out.printf(
-							"		번호		|		제목		|		내용		|		등록시간		|		조회수				\n");
+			} else if (command.startsWith("article list")) {
 
-					for (int i = list.size() - 1; i >= 0; i--) {
-						Articles article = list.get(i);
+				String searchKeyword = command.substring("article list".length()).trim();
+				List<Articles> forListArticles = list;
 
-						System.out.printf("		%d		|		%s		|		%s		|		%s		|		%d\n",
-								article.id, article.title, article.body, article.regDate, article.viewed);
+				if (searchKeyword.length() > 0) {
+					forListArticles = new ArrayList<>();
+					for (Articles article : list) {
+						if (article.title.contains(searchKeyword)) {
+							forListArticles.add(article);
+						}
 
 					}
 				}
+				if (list.size() == 0) {
+					System.out.print("현재 등록된 게시물이 없습니다. \n");
+					continue;
+				}
+				System.out.printf("		제목		|		제목		|		조회수		|		등록시간		\n");
 
-			} else if (command.startsWith("article detail")) {
+				for (int i = forListArticles.size() - 1; i >= 0; i--) {
+					Articles article = forListArticles.get(i);
+
+					System.out.printf("			%d		|		%s		|		%d		|		%s\n", article.id,
+							article.title, article.viewed, article.regDate);
+				}
+
+			}
+
+			else if (command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
 				int getId = Integer.parseInt(commandBits[2]);
 
@@ -100,37 +114,32 @@ public class Main {
 
 						fdarticle.title = title;
 						fdarticle.body = body;
-						
+
 						break;
 					}
 				}
 
-			}else if (command.startsWith("article delete")) {
+			} else if (command.startsWith("article delete ")) {
 				String[] commandBits = command.split(" ");
 				int getId = Integer.parseInt(commandBits[2]);
-				
+
 				Articles fdarticle = null;
-				
-				for(int i =0; i < list.size(); i++) {
+
+				for (int i = 0; i < list.size(); i++) {
 					fdarticle = list.get(i);
-					if(fdarticle.id == getId) {
+					if (fdarticle.id == getId) {
 						list.remove(i);
 						System.out.printf("%d번째 게시물이 삭제되었습니다\n", getId);
 						break;
 					}
 				}
-			}
-
-			else {
+			} else {
 				System.out.printf("%s(은)는 올바른 명령어가 아닙니다.\n", command);
 			}
 
 		}
-
 		sc.close();
-
-		System.out.println("====	프로그램 끝		====");
-
+		System.out.println("==== 프로그램 종료 ====");
 	}
 
 }
